6fb3577fc239616ec9f876ed9468906b795a24f9
DESKTOP-6KKTN16\william <williamferreiraladeira@hotmail.com>
2020-09-08 16:55:00 -0300

    configurar e alimentar dgv. controla parcialmente os botões

--- CRUD_Basico/CRUD_Basico/Forms/FormPrincipal.Designer.cs
+++ CRUD_Basico/CRUD_Basico/Forms/FormPrincipal.Designer.cs
@@ -28,16 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Dgvaluno = new System.Windows.Forms.DataGridView();
             this.BtnCadastrar = new System.Windows.Forms.Button();
             this.CkbAtivo = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
             this.DtpDtnascimento = new System.Windows.Forms.DateTimePicker();
             this.Txbnome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.TsbNovo = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Tsbsalvar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.TsbEditar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.TsbExcluir = new System.Windows.Forms.ToolStripButton();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgvaluno)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -45,7 +55,7 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.Dgvaluno);
             this.groupBox1.Controls.Add(this.BtnCadastrar);
             this.groupBox1.Controls.Add(this.CkbAtivo);
             this.groupBox1.Controls.Add(this.label2);
@@ -53,16 +63,35 @@
             this.groupBox1.Controls.Add(this.Txbnome);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(6, 12);
+            this.groupBox1.Location = new System.Drawing.Point(6, 28);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(755, 303);
+            this.groupBox1.Size = new System.Drawing.Size(449, 481);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Cadastro de Aluno";
             // 
+            // Dgvaluno
+            // 
+            this.Dgvaluno.AllowUserToAddRows = false;
+            this.Dgvaluno.AllowUserToDeleteRows = false;
+            this.Dgvaluno.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Dgvaluno.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.Dgvaluno.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgvaluno.Location = new System.Drawing.Point(29, 250);
+            this.Dgvaluno.MultiSelect = false;
+            this.Dgvaluno.Name = "Dgvaluno";
+            this.Dgvaluno.ReadOnly = true;
+            this.Dgvaluno.RowHeadersVisible = false;
+            this.Dgvaluno.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Dgvaluno.Size = new System.Drawing.Size(383, 228);
+            this.Dgvaluno.TabIndex = 7;
+            this.Dgvaluno.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dgvaluno_CellClick);
+            // 
             // BtnCadastrar
             // 
-            this.BtnCadastrar.Location = new System.Drawing.Point(120, 208);
+            this.BtnCadastrar.Location = new System.Drawing.Point(129, 208);
             this.BtnCadastrar.Name = "BtnCadastrar";
             this.BtnCadastrar.Size = new System.Drawing.Size(171, 36);
             this.BtnCadastrar.TabIndex = 6;
@@ -102,6 +131,7 @@
             this.Txbnome.Name = "Txbnome";
             this.Txbnome.Size = new System.Drawing.Size(381, 26);
             this.Txbnome.TabIndex = 1;
+            this.Txbnome.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txbnome_KeyDown);
             // 
             // label1
             // 
@@ -112,20 +142,87 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome:";
             // 
-            // dataGridView1
+            // toolStrip1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(447, 25);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(302, 261);
-            this.dataGridView1.TabIndex = 7;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TsbNovo,
+            this.toolStripSeparator1,
+            this.Tsbsalvar,
+            this.toolStripSeparator2,
+            this.TsbEditar,
+            this.toolStripSeparator3,
+            this.TsbExcluir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(467, 28);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // TsbNovo
+            // 
+            this.TsbNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.TsbNovo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TsbNovo.Image = ((System.Drawing.Image)(resources.GetObject("TsbNovo.Image")));
+            this.TsbNovo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.TsbNovo.Name = "TsbNovo";
+            this.TsbNovo.Size = new System.Drawing.Size(52, 25);
+            this.TsbNovo.Text = "Novo";
+            this.TsbNovo.Click += new System.EventHandler(this.TsbNovo_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 28);
+            // 
+            // Tsbsalvar
+            // 
+            this.Tsbsalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Tsbsalvar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tsbsalvar.Image = ((System.Drawing.Image)(resources.GetObject("Tsbsalvar.Image")));
+            this.Tsbsalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Tsbsalvar.Name = "Tsbsalvar";
+            this.Tsbsalvar.Size = new System.Drawing.Size(57, 25);
+            this.Tsbsalvar.Text = "Salvar";
+            this.Tsbsalvar.Click += new System.EventHandler(this.Tsbsalvar_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 28);
+            // 
+            // TsbEditar
+            // 
+            this.TsbEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.TsbEditar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TsbEditar.Image = ((System.Drawing.Image)(resources.GetObject("TsbEditar.Image")));
+            this.TsbEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.TsbEditar.Name = "TsbEditar";
+            this.TsbEditar.Size = new System.Drawing.Size(54, 25);
+            this.TsbEditar.Text = "Editar";
+            this.TsbEditar.Click += new System.EventHandler(this.TsbEditar_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 28);
+            // 
+            // TsbExcluir
+            // 
+            this.TsbExcluir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.TsbExcluir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TsbExcluir.Image = ((System.Drawing.Image)(resources.GetObject("TsbExcluir.Image")));
+            this.TsbExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.TsbExcluir.Name = "TsbExcluir";
+            this.TsbExcluir.Size = new System.Drawing.Size(59, 25);
+            this.TsbExcluir.Text = "Excluir";
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(773, 324);
+            this.ClientSize = new System.Drawing.Size(467, 518);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.groupBox1);
             this.Name = "FormPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
@@ -133,8 +230,11 @@
             this.Load += new System.EventHandler(this.FormPrincipal_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgvaluno)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -147,7 +247,15 @@
         private System.Windows.Forms.DateTimePicker DtpDtnascimento;
         private System.Windows.Forms.TextBox Txbnome;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView Dgvaluno;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton TsbNovo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton Tsbsalvar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton TsbEditar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton TsbExcluir;
     }
 }
 
--- CRUD_Basico/CRUD_Basico/Forms/FormPrincipal.cs
+++ CRUD_Basico/CRUD_Basico/Forms/FormPrincipal.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,27 +14,51 @@ namespace CRUD_Basico
 {
     public partial class FormPrincipal : Form
     {
+        private List<Aluno> _aluno;
+        private Aluno _alunoselecionado;
+
+
         public FormPrincipal()
         {
             InitializeComponent();
+            _aluno = new Aluno().obterAlunos();
         }
+
+        private void configuradgvaluno()
+        {
+            Dgvaluno.Columns.Add("id", "id");
+            Dgvaluno.Columns["id"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            Dgvaluno.Columns["id"].Visible = false;
+            Dgvaluno.Columns.Add("Nome", "Nome do Aluno");
+            Dgvaluno.Columns["nome"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            Dgvaluno.Columns.Add("dtnasc", "Data de Nacimento");
+            Dgvaluno.Columns["dtnasc"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+
+        }
+
+        private void carregaDgvalunos()
+        {
+            Dgvaluno.Rows.Clear();
+
+            foreach (Aluno aluno in _aluno)
+            {
+                Dgvaluno.Rows.Add(aluno .Id, aluno.Nome, aluno.DtNascimento.ToString("dd/MM/yyyy"));
+            }
+
+
+
+        }
+
+
+
 
         private void BtnCadastrar_Click(object sender, EventArgs e)
         {
            
 
-            try
-            {
-                Aluno novoAluno = new Aluno(Txbnome.Text, DtpDtnascimento.Value, CkbAtivo.Checked);
-
-                novoAluno.Cadastrar();
-                MessageBox.Show($"Aluno cadastrado com sucesso:\n {novoAluno.Nome}\nid inserido pelo banco:{novoAluno.Id}");
-            }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show(ex.Message);
-            }
+       
         }
 
         private void FormPrincipal_Load(object sender, EventArgs e)
@@ -41,14 +66,135 @@ namespace CRUD_Basico
             try
             {
 
-                List<Aluno> alunos = new Aluno().obterAlunos();
-                dataGridView1.DataSource = alunos;
+                //List<Aluno> alunos = new Aluno().obterAlunos();
+                //Dgvaluno.DataSource = alunos.Where (a => a.Ativo).ToList();
+
+                configuradgvaluno();
+                carregaDgvalunos();
+
             }
             catch (Exception ex)
             {
 
                 MessageBox.Show(ex.Message);
 
+            }
+        }
+
+        private void Dgvaluno_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+
+            _alunoselecionado = _aluno.Find(a => a.Id == (int)Dgvaluno.Rows[e.RowIndex].Cells["id"].Value);
+
+
+            Txbnome.Text = _alunoselecionado.Nome;
+            DtpDtnascimento.Value = _alunoselecionado.DtNascimento;
+            CkbAtivo.Checked = _alunoselecionado.Ativo;
+
+
+            configurabotoesecampos(3);
+
+        }
+
+        private void configurabotoesecampos(int estilo) 
+        {
+            if (estilo == 1)
+            {
+                Tsbsalvar.Enabled = false;
+                TsbEditar.Enabled = false;
+                TsbExcluir.Enabled = false;
+                TsbNovo.Enabled = true;
+
+
+                Txbnome.Clear();
+                Txbnome.Enabled = true;
+                CkbAtivo.Enabled = true;
+                CkbAtivo.Checked = true;
+                DtpDtnascimento.Enabled = true;
+
+
+                _alunoselecionado = null;
+            }
+            
+             else if (estilo == 2)
+             {
+                    Tsbsalvar.Enabled = true;
+                    TsbEditar.Enabled = false;
+                    TsbExcluir.Enabled = false;
+                    TsbNovo.Enabled = true;
+
+             }
+            else if (estilo == 3)
+            {
+                TsbNovo.Enabled = true;
+                Tsbsalvar.Enabled = false;
+                TsbEditar.Enabled = true;
+                TsbExcluir.Enabled = false;
+
+
+                Txbnome.Enabled = false;
+                DtpDtnascimento.Enabled = false;
+                CkbAtivo.Enabled = false;
+            }
+            else if (estilo == 4)
+            {
+                TsbNovo.Enabled = true;
+                Tsbsalvar.Enabled = true;
+                TsbExcluir.Enabled = false;
+                TsbEditar.Enabled = false;
+
+                Txbnome.Enabled = true;
+                DtpDtnascimento.Enabled = true;
+                CkbAtivo.Enabled = true;
+            }
+            
+        
+        }
+
+
+        private void TsbNovo_Click(object sender, EventArgs e)
+        {
+
+            configurabotoesecampos(1);
+
+
+        }
+
+        private void Txbnome_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Txbnome.Text.Trim().Length >= 2)
+            {
+                configurabotoesecampos(2);
+            }
+        }
+
+        private void TsbEditar_Click(object sender, EventArgs e)
+        {
+            configurabotoesecampos(4);
+        }
+
+        private void Tsbsalvar_Click(object sender, EventArgs e)
+        {
+            if (_alunoselecionado == null)
+            {
+                try
+                {
+                    Aluno novoAluno = new Aluno(Txbnome.Text, DtpDtnascimento.Value, CkbAtivo.Checked);
+
+                    novoAluno.Cadastrar();
+                    MessageBox.Show($"Aluno cadastrado com sucesso:\n {novoAluno.Nome}\nid inserido pelo banco:{novoAluno.Id}");
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                        
             }
         }
     }
